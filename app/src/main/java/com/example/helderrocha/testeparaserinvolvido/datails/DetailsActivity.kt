85bb0d82dfb2e0a550733e8f0662d5da36ee55c7
Heder Rocha <hedie.strokes@gmail.com>
2018-07-31 00:08:58 -0300

    load informations

--- app/src/main/java/com/example/helderrocha/testeparaserinvolvido/datails/DetailsActivity.kt
+++ app/src/main/java/com/example/helderrocha/testeparaserinvolvido/datails/DetailsActivity.kt
@@ -6,19 +6,26 @@ import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.Toast
 import com.arctouch.codechallenge.home.HomeAdapter
 import com.arctouch.codechallenge.home.MovieViewModel
 import com.arctouch.codechallenge.home.ViewModelFactory
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.helderrocha.testeparaserinvolvido.BR
 
 
 import com.example.helderrocha.testeparaserinvolvido.R
 import com.example.helderrocha.testeparaserinvolvido.databinding.DetailsActivityBinding
 import com.example.helderrocha.testeparaserinvolvido.model.Movie
+import com.example.helderrocha.testeparaserinvolvido.util.MovieImageUrlBuilder
 import dagger.android.AndroidInjection
 import io.reactivex.disposables.CompositeDisposable
+import kotlinx.android.synthetic.main.details_activity.*
 import kotlinx.android.synthetic.main.home_activity.*
+import kotlinx.android.synthetic.main.movie_item.view.*
 import javax.inject.Inject
 
 class DetailsActivity : AppCompatActivity() {
@@ -31,6 +38,7 @@ class DetailsActivity : AppCompatActivity() {
 
 
     private val movieObserver = Observer<Movie>(::onMovieFetched)
+    private  lateinit var img : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -41,6 +49,7 @@ class DetailsActivity : AppCompatActivity() {
         val data: Bundle = intent.extras
         var movieId = data.getInt("movie_selected")
 
+        img = findViewById<View>(R.id.imageBackground) as ImageView
         movieViewModel.movie.observe(this, movieObserver)
         movieViewModel.getMovieById(movieId.toLong())
 
@@ -52,7 +61,15 @@ class DetailsActivity : AppCompatActivity() {
         val binding: DetailsActivityBinding =  DataBindingUtil.setContentView(this, R.layout.details_activity)
 
         val _movie = movie
+        val movieImageUrlBuilder = MovieImageUrlBuilder()
         binding.setVariable(BR.movie, movie)
+        if (movie != null) {
+            Glide.with(img)
+                    .load(movie.backdropPath?.let { movieImageUrlBuilder.buildPosterUrl(it) })
+                    .apply(RequestOptions().placeholder(R.drawable.ic_image_placeholder))
+                    .into(img.posterImageView)
+        }
+
         binding.executePendingBindings()
     }
 
