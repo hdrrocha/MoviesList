3a98160feed67c3a471d2177b38e81e6115d9713
Helder Rocha <hedie.strokes@gmail.com>
2018-07-31 16:41:02 -0300

    loading

--- app/src/main/java/com/example/helderrocha/testeparaserinvolvido/datails/DetailsActivity.kt
+++ app/src/main/java/com/example/helderrocha/testeparaserinvolvido/datails/DetailsActivity.kt
@@ -6,10 +6,6 @@ import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.Toast
-import com.arctouch.codechallenge.home.HomeAdapter
 import com.arctouch.codechallenge.home.MovieViewModel
 import com.arctouch.codechallenge.home.ViewModelFactory
 import com.bumptech.glide.Glide
@@ -22,18 +18,15 @@ import com.example.helderrocha.testeparaserinvolvido.databinding.DetailsActivity
 import com.example.helderrocha.testeparaserinvolvido.model.Movie
 import com.example.helderrocha.testeparaserinvolvido.util.MovieImageUrlBuilder
 import dagger.android.AndroidInjection
-import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.details_activity.*
 import kotlinx.android.synthetic.main.home_activity.*
-import kotlinx.android.synthetic.main.movie_item.*
-import kotlinx.android.synthetic.main.movie_item.view.*
 import javax.inject.Inject
 
 class DetailsActivity : AppCompatActivity() {
     @Inject
     lateinit var movieVMFactory: ViewModelFactory<MovieViewModel>
 
-    private  val  movieViewModel by lazy {
+    private val movieViewModel by lazy {
         ViewModelProviders.of(this, movieVMFactory)[MovieViewModel::class.java]
     }
 
@@ -55,20 +48,14 @@ class DetailsActivity : AppCompatActivity() {
     }
 
     private fun onMovieFetched(movie: Movie?) {
-        Toast.makeText(this, "Movie: ${movie?.backdropPath}", Toast.LENGTH_LONG).show()
-
-        val binding: DetailsActivityBinding =  DataBindingUtil.setContentView(this, R.layout.details_activity)
-
-        val _movie = movie
+        val binding: DetailsActivityBinding = DataBindingUtil.setContentView(this, R.layout.details_activity)
         val movieImageUrlBuilder = MovieImageUrlBuilder()
         binding.setVariable(BR.movie, movie)
         if (movie != null) {
-//            titleTextView.text = movie.title
-//            yearMovie.text = movie.releaseDate
             genresDetaisTextView.text = movie.genres?.joinToString(separator = ", ") { it.name }
 
             Glide.with(this)
-                    .load(movie.backdropPath?.let { movieImageUrlBuilder.buildPosterUrl(it) })
+                    .load(movie.backdropPath?.let { movieImageUrlBuilder.buildBackdropUrl(it) })
                     .apply(RequestOptions().placeholder(R.drawable.ic_image_placeholder))
                     .into(imageBackground)
 
@@ -76,9 +63,8 @@ class DetailsActivity : AppCompatActivity() {
                     .load(movie.posterPath?.let { movieImageUrlBuilder.buildPosterUrl(it) })
                     .apply(RequestOptions().placeholder(R.drawable.ic_image_placeholder))
                     .into(imagCover)
+            progressBarDetails.visibility = View.GONE
         }
-
-//        binding.executePendingBindings()
     }
 
 }
