f2b2356d688c0e180ec894a228c4f5fb081172ae
Helder Rocha <hedie.strokes@gmail.com>
2018-08-01 10:39:09 -0300

    gambi scroll

--- app/src/main/java/com/example/helderrocha/testeparaserinvolvido/home/HomeActivity.kt
+++ app/src/main/java/com/example/helderrocha/testeparaserinvolvido/home/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.example.helderrocha.testeparaserinvolvido.home
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
@@ -13,12 +14,18 @@ import android.widget.Toast
 import com.arctouch.codechallenge.home.HomeAdapter
 import com.arctouch.codechallenge.home.MoviesViewModel
 import com.arctouch.codechallenge.home.ViewModelFactory
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.helderrocha.testeparaserinvolvido.BR
 import com.example.helderrocha.testeparaserinvolvido.R
 
 import com.example.helderrocha.testeparaserinvolvido.R.*
+import com.example.helderrocha.testeparaserinvolvido.databinding.DetailsActivityBinding
 import com.example.helderrocha.testeparaserinvolvido.datails.DetailsActivity
 import com.example.helderrocha.testeparaserinvolvido.model.Movie
+import com.example.helderrocha.testeparaserinvolvido.util.MovieImageUrlBuilder
 import dagger.android.AndroidInjection
+import kotlinx.android.synthetic.main.details_activity.*
 import kotlinx.android.synthetic.main.home_activity.*
 import javax.inject.Inject
 
@@ -30,28 +37,34 @@ class HomeActivity : AppCompatActivity() {
         ViewModelProviders.of(this, moviesVMFactory)[MoviesViewModel::class.java]
     }
     private var itensList: Int = 0
+    private lateinit var adapter: HomeAdapter
+    protected var listMovies: ArrayList<Movie> = ArrayList()
 
+    protected val moviesObserver = Observer<List<Movie>>(::onMoviesFetched)
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_activity)
 
-        moviesViewModel.getData().observe(this, Observer(updateList))
         val layoutManager = LinearLayoutManager(this)
         recyclerView.layoutManager = layoutManager
 
+        moviesViewModel.getData().observe(this, Observer(updateList))
+        moviesViewModel.movies.observe(this, moviesObserver)
+
+        adapter =  HomeAdapter(listMovies, { movie: Movie -> partItemClicked(movie) })
         val listScrollLister = object : RecyclerView.OnScrollListener() {
             override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
                 super.onScrolled(recyclerView, dx, dy)
-
-                Log.i("Helder", "layoutManager.itemCount" + layoutManager.itemCount)
-                Log.i("Helder", "layoutManager.findLastVisibleItemPosition()" + layoutManager.findLastVisibleItemPosition())
-                var currency = layoutManager.findLastVisibleItemPosition()
-                if(currency+1 >= itensList){
+                var currency = layoutManager.findLastVisibleItemPosition() + 1
+                if(currency >= itensList){
                     Log.i("Helder", "Posso carregar mais")
-                    moviesViewModel.getData().observe(this@HomeActivity, Observer(updateList))
+                    Log.i("Helder", "itensList" + itensList)
+                    Log.i("Helder", "currency" + currency)
+                    moviesViewModel.getMoreMovies()
+//                    moviesViewModel.getData().observe(this@HomeActivity, Observer(updateList))
+
                 }
-//
             }
         }
 
@@ -59,12 +72,23 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private val updateList: (List<Movie>?) -> Unit = {
-        recyclerView.adapter = HomeAdapter((it as ArrayList<Movie>?)!!, { movie : Movie -> partItemClicked(movie) })
+        listMovies.addAll((it as ArrayList<Movie>?)!!)
+        adapter.addAll(listMovies)
+        recyclerView.adapter = adapter
         itensList = recyclerView.adapter.itemCount
-        Log.i("Helder", "iniciei com:"+ itensList)
         progressBar.visibility = View.GONE
     }
 
+    private fun onMoviesFetched(newMovies: List<Movie>?) {
+        if (newMovies != null) {
+            listMovies.clear()
+            listMovies.addAll(newMovies)
+            adapter.addAll(listMovies)
+            adapter.notifyDataSetChanged()
+            itensList = recyclerView.adapter.itemCount
+        }
+
+    }
     private fun partItemClicked(movie : Movie) {
         val showDetailActivityIntent = Intent(this, DetailsActivity::class.java)
         showDetailActivityIntent.putExtra("movie_selected", movie.id)
--- app/src/main/java/com/example/helderrocha/testeparaserinvolvido/home/Movies.kt
+++ app/src/main/java/com/example/helderrocha/testeparaserinvolvido/home/Movies.kt
@@ -21,17 +21,44 @@ class ViewModelFactory<VM : ViewModel> @Inject constructor(private val viewModel
     }
 }
 
-class MoviesViewModel @Inject constructor(api: ApiClient, private val schedulers: SchedulerProvider) : ViewModel() {
+class MoviesViewModel @Inject constructor(val api: ApiClient, private val schedulers: SchedulerProvider) : ViewModel() {
     private val moviesData = MoviesLiveData(api, schedulers)
 
     fun getData(): LiveData<List<Movie>> = moviesData
+    var page = 1L
+
+    val _movies = MutableLiveData<List<Movie>>()
+    val movies: LiveData<List<Movie>> = _movies
+
+    fun getMoreMovies() {
+        page++
+        Log.i("Helder", "ViewModel page" + page)
+        api.genres(TmdbApi.API_KEY, TmdbApi.DEFAULT_LANGUAGE)
+                .doOnSuccess {
+                    Cache.cacheGenres(it.genres)
+                }
+                .flatMapObservable {
+
+                    api.upcomingMovies(TmdbApi.API_KEY, TmdbApi.DEFAULT_LANGUAGE, page, TmdbApi.DEFAULT_REGION)
+                }
+                .subscribeOn(schedulers.io())
+                .observeOn(schedulers.mainThread())
+                .subscribe({
+                    _movies.value = it.results.map { movie ->
+                        movie.copy(genres = Cache.genres.filter { movie.genreIds?.contains(it.id) == true })
+                    }
+                }, {
+                    _movies.value = listOf()
+                })
+    }
+
+
 }
 
 class MoviesLiveData(private val api: ApiClient, private val schedulers: SchedulerProvider) : LiveData<List<Movie>>() {
-    var page = 0L
+    var page = 1L
+
     init {
-        page++
-        Log.i("Helder", "ViewModel page" + page)
         loadData()
     }
 
